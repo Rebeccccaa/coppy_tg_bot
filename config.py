aeb914ea2146467507c6d8c47c6ad2efe626ebba
Rebeccccaa <gborisov01@hotmail.com>
2025-09-18 14:56:13 +0300

    18.09.2025

---
+++ config.py
@@ -0,0 +1,11 @@
+api_id = 29675143
+api_hash = "1833842cc7691a7a16634adf2d9bd3e2"
+bot_token = "8312054210:AAHsNFCbFTkSxeTdE5KiCwN6qOKw3oeMYps"
+  
+my_id = 6800680895
+technical_channel_id = -1003027929238
+new_link = "http://t.me/"
+new_username = "@"
+proxy_url = "http://ip:port"
+openai_api_key = "sk-proj-jdwadadwadwadawdwWGFRGSFESFESFESagfegrgdgrg"
+
